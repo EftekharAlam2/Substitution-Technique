0e4e2e49fd7f08f1dbbded855b7b229a1c5b3469
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-10-29 11:13:36 +0600

    Caesar cipher encryption added

---
+++ Caesar-Cipher/Caesar-Cipher-Encryption.cpp
@@ -0,0 +1,39 @@
+//Encryption
+
+#include<iostream>
+
+using namespace std;
+
+int main () {
+  char message[100], ch;
+  int i, key;
+  cout << "Enter the plaintext: ";
+  cin.getline(message, 100);
+  
+  cout << "Enter the key: ";
+  cin >> key;
+
+  for (i=0; message[i] != '\0'; ++i){
+    ch = message[i];
+
+    if (ch >= 'a' && ch <= 'z'){
+      ch = ch + key;
+      if(ch > 'z'){
+        ch = ch - 'z' + 'a' -1;
+      }
+      message[i] = ch;
+    }
+
+    else if(ch >= 'A' && ch <= 'Z'){
+      ch = ch + key;
+      if(ch > 'Z'){
+        ch = ch - 'Z' + 'A' - 1;
+      }
+      message[i] = ch;
+    }
+  }
+
+  cout << "Encrypted message: " << message;
+
+  return 0;
+}
